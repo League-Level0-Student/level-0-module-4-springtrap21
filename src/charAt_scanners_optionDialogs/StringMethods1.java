03f5f52df514f58fa51cb04d03116be37c579052
ilabstaff <ilabstaff@172.16.116.68>
2019-04-03 19:28:21 -0700

    ChangeVol.2...&stringMethod

--- src/charAt_scanners_optionDialogs/StringMethods1.java
+++ src/charAt_scanners_optionDialogs/StringMethods1.java
@@ -8,23 +8,29 @@ public class StringMethods1 {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to whatever you want
-
+		 String bozo = "Where are my pants ? HEY ! Why did you throw them in the sewer ! Now they belong to the Bozos !";
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-		
+		 System.out.println(bozo.charAt(2));
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
-
+		  System.out.println (bozo.length());
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
+		  for (int i = 0; i < bozo.length(); i++) {
+			System.out.println(bozo.charAt(i));
+		}
 		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
 		//    Print the char's position to the console.
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
-		
+		  for (int i = 0; i < bozo.length(); i++) {
+			if ("t".charAt(0) == (bozo.charAt(i))) {
+				System.out.println("t is at index " + i);
+			}
+		}
 	}
 }
 
--- src/doubles_and_booleans/ChangeCalculator.java
+++ src/doubles_and_booleans/ChangeCalculator.java
@@ -3,6 +3,8 @@ package doubles_and_booleans;
 //    Level 0
 
 
+import java.text.DecimalFormat;
+
 import javax.swing.JOptionPane;
 
 /*
@@ -16,16 +18,19 @@ public class ChangeCalculator {
 
 		// Ask the user how many nickels they have
 		String smg = JOptionPane.showInputDialog("How many of ze nikels do you have? TELL ME!!!");
-		// Convert their answer to an int using Integer.parseInt()
-		int nukkles = Integer.parseInt(smg )
+		// Convert their answer to an in using Integer.parseInt()
+		int nukkles = Integer.parseInt( smg );
 		// Ask the user how many dimes they have, and convert their answer
-
+		String lmg = JOptionPane.showInputDialog("How many of ze dimes do you have? TELL ME NOW!!!");
+		int hobo = Integer.parseInt( lmg );
 		// Ask the user how many quarters they have, and convert their answer
-
+		String bozo = JOptionPane.showInputDialog("How many of ze quarters do you have? I Will Find You!");
+		int hipi = Integer.parseInt( bozo );
 		// Calculate how much money the user has and save it in a double variable 
-
+		double bigMac = (nukkles * 5 + hobo * 10 + hipi * 25);
+		bigMac = bigMac / 100 ;
 		// Tell the user how much money they have
-
+		JOptionPane.showMessageDialog(null, "Your total is $" + bigMac + " Look behind you!");
 	}
 }
 
