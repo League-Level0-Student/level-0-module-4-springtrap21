46a1560c46bfc15d4942324afa7586ce3ba8a8b0
ilabstaff <ilabstaff@172.16.116.71>
2019-04-10 19:30:08 -0700

    stringMeth1,2

--- src/charAt_scanners_optionDialogs/StringMethods1.java
+++ src/charAt_scanners_optionDialogs/StringMethods1.java
@@ -35,3 +35,4 @@ public class StringMethods1 {
 }
 
 
+   
--- src/extra/StringMethods2.java
+++ src/extra/StringMethods2.java
@@ -8,18 +8,21 @@ public class StringMethods2 {
 		
 		// 1. Create a String variable and initialize it with
 		//    random upper and lower case characters.
-
+		String boi = "YoRhA";
 		// 2. Print your String to the console in upper case.
-		
+		System.out.println(boi.toUpperCase());
 		// 3. Print your String to the console in lower case.
-
+		System.out.println(boi.toLowerCase());
 		// 4. Print the first 3 char's of your String
 		//    HINT: .substring(start,end)
-		
+		System.out.println(boi.substring(0, 3));
 		// 5. Print a single char somewhere in the middle of your String
 		//    REMINDER: char's in string start at index 0
-		
+		System.out.println(boi.charAt(3));
 		// 6. BONUS -- print the LAST 3 char's of your string using
 		//        .length() to determine WHERE the last 3 char's are located.
-	}
+		System.out.println(boi.substring(boi.length()-3,boi.length()));
+	
+	
+	}	
 }
